8a5c8c67c0aa2b58c7bc44224b4f1e500bdb8eb9
mcmalloy <xomagametv@gmail.com>
2019-10-07 11:23:16 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marksgalgeleg/GameActivity.java
@@ -0,0 +1,32 @@
+package com.example.marksgalgeleg;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.marksgalgeleg.Galgelogik;
+
+public class GameActivity extends AppCompatActivity {
+
+    /**
+     * TODO:
+     * Create the actual game screen
+     * Show the first picture of the empty noose
+     * Show the form of hidden word
+     *
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/marksgalgeleg/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.marksgalgeleg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button Startbutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Startbutton = (Button) findViewById(R.id.StartGameButton);
+
+        // Capture button clicks
+        Startbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                startGame();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    public void startGame(){
+        // Starts the game activity. Let the games commence
+        Intent myIntent = new Intent(this,
+                GameActivity.class);
+        startActivity(myIntent);
+    }
+
+
+}
