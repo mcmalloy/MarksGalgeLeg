2f5d6e20753278658945108af85918ec73995569
Mark Malloy <markmalloy96@yahoo.dk>
2019-10-21 07:08:29 +0200

    Restructured GameActivity and created a method makeGuess, for handling the users guess input

--- app/src/main/java/com/example/marksgalgeleg/GameActivity.java
+++ app/src/main/java/com/example/marksgalgeleg/GameActivity.java
@@ -1,4 +1,5 @@
 package com.example.marksgalgeleg;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,28 +11,10 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
-
-  //TODO: Show the word in its hidden form, displaying each correctly guessed word
-
-
     Button gætKnap;
+    final Galgelogik spil = new Galgelogik();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ImageView img;
-        final int[] images =
-                {
-                        R.drawable.galge,
-                        R.drawable.forkert1,
-                        R.drawable.forkert2,
-                        R.drawable.forkert3,
-                        R.drawable.forkert4,
-                        R.drawable.forkert5,
-                        R.drawable.forkert6
-                };
-        final int[] currentImage = {0};
-
-        final Galgelogik spil = new Galgelogik();
         spil.nulstil();
 
         super.onCreate(savedInstanceState);
@@ -48,7 +31,6 @@ public class GameActivity extends AppCompatActivity {
         gætKnap.setOnClickListener(new View.OnClickListener() { // Lytter til når brugeren trykker på gæt knap.
 
             public void onClick(View arg0) {
-
                 String bogstav = String.valueOf(bogstavGættet.getText());
                 spil.gætBogstav(bogstav);
                 spil.logStatus();
@@ -59,22 +41,45 @@ public class GameActivity extends AppCompatActivity {
                 synligtOrd.setText("" + ord);
                 System.out.println("Spil status: "+spil.erSpilletSlut());
 
-                if (spil.erSpilletSlut()!=true && spil.erSidsteBogstavKorrekt()==false) {
-                    ImageView image = (ImageView) findViewById(R.id.gameImage);
-                    image.setImageResource(images[++currentImage[0]]);
-                    addWrongLetter(bogstav);
-                }
-                else{
-                    addCorrectLetter(bogstav);
-                    if(spil.erSpilletVundet()){
-                        Toast.makeText(getApplicationContext(),"Du har vundet spillet!",Toast.LENGTH_LONG);
-                    }
-                    else if(spil.erSpilletTabt()){
-                        Toast.makeText(getApplicationContext(),"Du har tabt spillet!",Toast.LENGTH_LONG);
-                    }
-                }
+                makeGuess(bogstav);
             }
         });
+    }
+
+    public void makeGuess(String bogstav){
+        final int[] images =
+                {
+                        R.drawable.galge,
+                        R.drawable.forkert1,
+                        R.drawable.forkert2,
+                        R.drawable.forkert3,
+                        R.drawable.forkert4,
+                        R.drawable.forkert5,
+                        R.drawable.forkert6
+                };
+        final int[] currentImage = {0};
+
+
+
+        // After every guess we check to see if the game has now concluded.
+        if(spil.erSpilletSlut()){
+            if(spil.erSpilletVundet()){
+                // Sends the user to the winpage
+                Intent myIntent = new Intent(this,
+                        WinActivity.class);
+                startActivity(myIntent);
+            }
+            //TODO : Also create a losingState activity/fragment (will use fragments in next iteration)
+        }
+        else if(spil.erSidsteBogstavKorrekt()){
+            addCorrectLetter(bogstav);
+        }
+        else if(spil.erSidsteBogstavKorrekt()==false){
+            ImageView image = (ImageView) findViewById(R.id.gameImage);
+            image.setImageResource(images[++currentImage[0]]);
+            addWrongLetter(bogstav);
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/marksgalgeleg/WinActivity.java
@@ -0,0 +1,22 @@
+package com.example.marksgalgeleg;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WinActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_winstate);
+
+
+
+
+    }
+}
