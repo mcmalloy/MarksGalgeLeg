2cd5b09b306b6d3c3dda8a0cd0fbdd2362c297de
Mark Malloy <markmalloy96@yahoo.dk>
2019-10-21 08:02:51 +0200

    Restructured GameActivity and created a method makeGuess, for handling the users guess input

--- app/src/main/java/com/example/marksgalgeleg/GameActivity.java
+++ app/src/main/java/com/example/marksgalgeleg/GameActivity.java
@@ -6,15 +6,27 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameActivity extends AppCompatActivity {
     Button gætKnap;
     final Galgelogik spil = new Galgelogik();
+    final int[] images =
+            {
+                    R.drawable.galge,
+                    R.drawable.forkert1,
+                    R.drawable.forkert2,
+                    R.drawable.forkert3,
+                    R.drawable.forkert4,
+                    R.drawable.forkert5,
+                    R.drawable.forkert6
+            };
+    final int[] currentImage = {0};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         spil.nulstil();
 
         super.onCreate(savedInstanceState);
@@ -23,51 +35,36 @@ public class GameActivity extends AppCompatActivity {
         final TextView debugWord = (TextView) findViewById(R.id.ActualWord);
         final TextView synligtOrd = (TextView) findViewById(R.id.synligtord);
 
-        debugWord.append("DEBUG, Ordet i denne runde er: "+ spil.getOrdet());
-        synligtOrd.setText(spil.getSynligtOrd());
+            debugWord.append("DEBUG, Ordet i denne runde er: "+ spil.getOrdet());
+            synligtOrd.setText(spil.getSynligtOrd());
 
-        gætKnap = (Button) findViewById(R.id.GuessButton);
-        // Capture button clicks
-        gætKnap.setOnClickListener(new View.OnClickListener() { // Lytter til når brugeren trykker på gæt knap.
+            gætKnap = (Button) findViewById(R.id.GuessButton);
+            // Capture button clicks
+            gætKnap.setOnClickListener(new View.OnClickListener() { // Lytter til når brugeren trykker på gæt knap.
 
-            public void onClick(View arg0) {
-                String bogstav = String.valueOf(bogstavGættet.getText());
-                spil.gætBogstav(bogstav);
-                spil.logStatus();
+                public void onClick(View arg0) {
+                    String bogstav = String.valueOf(bogstavGættet.getText());
+                    spil.gætBogstav(bogstav);
+                    spil.logStatus();
 
-                bogstavGættet.setText(""); // Resetter tekstfeltet så snart brugeren har lavet et gæt
-                String ord = spil.getSynligtOrd();
-                System.out.println(ord);
-                synligtOrd.setText("" + ord);
-                System.out.println("Spil status: "+spil.erSpilletSlut());
+                    bogstavGættet.setText(""); // Resetter tekstfeltet så snart brugeren har lavet et gæt
+                    String ord = spil.getSynligtOrd();
+                    System.out.println(ord);
+                    synligtOrd.setText("" + ord);
+                    System.out.println("Spil status: "+spil.erSpilletSlut());
 
-                makeGuess(bogstav);
-            }
-        });
+                    makeGuess(bogstav);
+                }
+            });
+
     }
 
     public void makeGuess(String bogstav){
-        final int[] images =
-                {
-                        R.drawable.galge,
-                        R.drawable.forkert1,
-                        R.drawable.forkert2,
-                        R.drawable.forkert3,
-                        R.drawable.forkert4,
-                        R.drawable.forkert5,
-                        R.drawable.forkert6
-                };
-        final int[] currentImage = {0};
-
-
 
         // After every guess we check to see if the game has now concluded.
         if(spil.erSpilletSlut()){
             if(spil.erSpilletVundet()){
-                // Sends the user to the winpage
-                Intent myIntent = new Intent(this,
-                        WinActivity.class);
-                startActivity(myIntent);
+                goToWinScreen(); // Sends the user to the winpage
             }
             //TODO : Also create a losingState activity/fragment (will use fragments in next iteration)
         }
@@ -79,8 +76,6 @@ public class GameActivity extends AppCompatActivity {
             image.setImageResource(images[++currentImage[0]]);
             addWrongLetter(bogstav);
         }
-
-
     }
 
     public void addWrongLetter(String letter){
@@ -91,5 +86,12 @@ public class GameActivity extends AppCompatActivity {
     public void addCorrectLetter(String letter){
         final TextView brugteBogstaverKorrekt = (TextView) findViewById(R.id.KorrekteBogstaver);
         brugteBogstaverKorrekt.append(" "+letter);
+    }
+
+    public void goToWinScreen(){
+        // Sends the user to the winpage
+        Intent myIntent = new Intent(this,
+                WinActivity.class);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/com/example/marksgalgeleg/WinActivity.java
+++ app/src/main/java/com/example/marksgalgeleg/WinActivity.java
@@ -1,5 +1,7 @@
 package com.example.marksgalgeleg;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,13 +12,46 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WinActivity extends AppCompatActivity {
-
+    Button RestartGameButton;
+    Button ReturnMainMenuButton;
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winstate);
 
+        listenToButtons();
+
+    }
 
 
+    public void listenToButtons(){
+        // Capture button clicks
+        //TODO: Currently  this activity does not allow buttons. Returns a nullpointerexception??
+//        RestartGameButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View arg0) {
+//                System.out.println("play again");
+//                playAgain();
+//            }
+//        });
+//        ReturnMainMenuButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View arg0) {
+//                System.out.println("main menu");
+//                GotoMenu();
+//            }
+//        });
+    }
 
+    public void playAgain(){
+        // Sends the user to a new game
+        Intent myIntent = new Intent(this,
+                GameActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void GotoMenu(){
+    // Sends the user to main menu
+        Intent myIntent = new Intent(this,
+                MainActivity.class);
+        startActivity(myIntent);
     }
 }
